0cfedc3a08b5d572bccbb55fc981dfb0cb23d6b8
Yuval Segal <yu.segal@gmail.com>
2022-07-11 21:51:49 +0300

    finshed HistoryController

--- src/main/java/org/bgu/ise/ddb/history/HistoryController.java
+++ src/main/java/org/bgu/ise/ddb/history/HistoryController.java
@@ -3,7 +3,12 @@
  */
 package org.bgu.ise.ddb.history;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,8 +24,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOptions;
 
 /**
  * @author Alex
@@ -50,50 +61,65 @@ public class HistoryController extends ParentController{
 
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
 		MongoDatabase db = mongoClient.getDatabase("FinalP");
-		MongoCollection<Document> usrH = db.getCollection("UsersHistory");
+		MongoCollection<Document> userH = db.getCollection("UsersHistory");
 		MongoCollection<Document> titleH = db.getCollection("TitleHistory");
+		MongoCollection<Document> usersCo = db.getCollection("UsersCollection");
+
+		Document tempDoc = new Document("username", username);
+		MongoCursor<Document> srcRes = usersCo.find(tempDoc).iterator();
+		if(!srcRes.hasNext()) {
+			response.setStatus(status.value());
+			mongoClient.close();
+			System.out.println("\nNO USERNAME FOUND!\n");
+			return;
+		}
 		int flag = 0;
 		
 			
 		try {
-			if (/*first insert to title*/) {
-				Date date = new Date();
-			    long timeMilli = date.getTime();
-
-				Document doc = new Document("_id", title);
-	            doc.append("username", username);
-	            doc.append("timestamp", timeMilli);
-	            
-	            Document query = new Document("_id", title);
-				titleH.updateOne(query, doc);
-				status = HttpStatus.OK;
-			} 
-			else {
-			//already exists
-			}
+//			insert to title indexed collection
+			Date date = new Date();
+		    long timeMilli = date.getTime();
+		    
+		    
+		    Document doc2 = new Document("username", username);
+            doc2.append("timestamp", timeMilli);
+            List<Document> history = new ArrayList<Document>();
+            history.add(doc2);
+			Document doc = new Document("history", history);
+           
 			
-			if (/*first insert to user*/) {
-				Date date = new Date();
-						
-				Document doc = new Document("username", username);
-	            doc.append("password", password);
-	            doc.append("firstName", firstName);
-	            doc.append("lastName", lastName);
-	            doc.append("addTime", date);
-				usr.insertOne(doc);
-				status = HttpStatus.OK;
-			} 
-			else {
-				//already exists
-			}
+            Document query = new Document("_id", title);
+            UpdateOptions options = new UpdateOptions().upsert(true);
+
+            titleH.updateOne(query, new Document("$push",doc), options);
+
+            flag++;
+            
+//			insert to user indexed collection
+		    
+		    doc2 = new Document("title", title);
+            doc2.append("timestamp", timeMilli);
+            history = new ArrayList<Document>();
+            history.add(doc2);
+			doc = new Document("history", history);
+           
+			
+            query = new Document("_id", username);
+            options = new UpdateOptions().upsert(true);
+
+            userH.updateOne(query, new Document("$push",doc), options);
+
+            flag++;
 		}
 		catch (MongoException me) {
             System.err.println("Unable to insert due to an error: " + me);
 		}
-		
+		//TODO change flag to 2 
 		if(flag == 2) {
 			status = HttpStatus.OK;
 		}
+		mongoClient.close();
 		response.setStatus(status.value());
 
 	}
@@ -110,9 +136,51 @@ public class HistoryController extends ParentController{
 	@org.codehaus.jackson.map.annotate.JsonView(HistoryPair.class)
 	public  HistoryPair[] getHistoryByUser(@RequestParam("entity")    String username){
 		//:TODO your implementation
-		HistoryPair hp = new HistoryPair("aa", new Date());
-		System.out.println("ByUser "+hp);
-		return new HistoryPair[]{hp};
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		MongoDatabase db = mongoClient.getDatabase("FinalP");
+		MongoCollection<Document> userH = db.getCollection("UsersHistory");
+        Document query = new Document("_id", username);
+
+        AggregateIterable<Document> findIterable = userH.aggregate(
+                Arrays.asList(
+                        new Document("$match", query),
+                        new Document("$unwind", "$history"),
+                        new Document("$sort", new Document("history.timestamp", -1))
+                    )   
+            );
+        int j = 0;
+        System.out.println("\n\nHello\n\n");
+        System.out.println(findIterable.first().toJson());
+        System.out.println("Hello\n\n");
+
+        for(Document d : findIterable)
+        {
+
+        	@SuppressWarnings("unchecked")
+			ArrayList<Document> tmp = (ArrayList<Document>) d.get("history");
+            for (Document i : tmp)
+            {
+                System.out.println(i.toJson());
+            	j++;
+            }
+        }
+
+        HistoryPair[] res = new HistoryPair[j];
+        j = 0;
+        for(Document d : findIterable)
+        {
+
+        	@SuppressWarnings("unchecked")
+			ArrayList<Document> tmp = (ArrayList<Document>) d.get("history");
+            for (Document i : tmp)
+            {
+            	HistoryPair hp = new HistoryPair((String)i.get("title"), new Date((long)i.get("timestamp")));
+            	res[j] = hp;
+            	j++;
+            }
+        }
+        mongoClient.close();
+		return res;
 	}
 	
 	
@@ -127,9 +195,47 @@ public class HistoryController extends ParentController{
 	@org.codehaus.jackson.map.annotate.JsonView(HistoryPair.class)
 	public  HistoryPair[] getHistoryByItems(@RequestParam("entity")    String title){
 		//:TODO your implementation
-		HistoryPair hp = new HistoryPair("aa", new Date());
-		System.out.println("ByItem "+hp);
-		return new HistoryPair[]{hp};
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		MongoDatabase db = mongoClient.getDatabase("FinalP");
+		MongoCollection<Document> titleH = db.getCollection("TitleHistory");
+        Document query = new Document("_id", title);
+
+        AggregateIterable<Document> findIterable = titleH.aggregate(
+                Arrays.asList(
+                        new Document("$match", query),
+                        new Document("$unwind", "$history"),
+                        new Document("$sort", new Document("history.timestamp", -1))
+                    )   
+            );
+        int j = 0;
+
+        for(Document d : findIterable)
+        {
+
+        	@SuppressWarnings("unchecked")
+			ArrayList<Document> tmp = (ArrayList<Document>) d.get("history");
+            for (Document i : tmp)
+            {
+            	j++;
+            }
+        }
+
+        HistoryPair[] res = new HistoryPair[j];
+        j = 0;
+        for(Document d : findIterable)
+        {
+
+        	@SuppressWarnings("unchecked")
+			ArrayList<Document> tmp = (ArrayList<Document>) d.get("history");
+            for (Document i : tmp)
+            {
+            	HistoryPair hp = new HistoryPair((String)i.get("username"), new Date((long)i.get("timestamp")));
+            	res[j] = hp;
+            	j++;
+            }
+        }
+        mongoClient.close();
+		return res;
 	}
 	
 	/**
@@ -142,9 +248,47 @@ public class HistoryController extends ParentController{
 	@org.codehaus.jackson.map.annotate.JsonView(HistoryPair.class)
 	public  User[] getUsersByItem(@RequestParam("title") String title){
 		//:TODO your implementation
-		User hp = new User("aa","aa","aa");
-		System.out.println(hp);
-		return new User[]{hp};
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		MongoDatabase db = mongoClient.getDatabase("FinalP");
+		MongoCollection<Document> titleH = db.getCollection("TitleHistory");
+		MongoCollection<Document> usersCo = db.getCollection("UsersCollection");
+
+        Document query = new Document("_id", title);
+
+        AggregateIterable<Document> findIterable = titleH.aggregate(
+                Arrays.asList(
+                        new Document("$match", query),
+                        new Document("$unwind", "$history")
+                    )   
+            );
+
+        Set<String> set = new HashSet<String> (); 
+
+        for(Document d : findIterable)
+        {
+        	@SuppressWarnings("unchecked")
+			ArrayList<Document> tmp = (ArrayList<Document>) d.get("history");
+            for (Document i : tmp)
+            {
+            	set.add(i.getString("username"));
+            }
+        }
+        
+        User[] res = new User[set.size()];
+        int j = 0;
+        for(String username : set)
+        {
+        	Document doc = new Document("username", username);
+    		MongoCursor<Document> srcRes = usersCo.find(doc).iterator();
+			Document tmp = srcRes.next();
+			User hp = new User(username,(String)tmp.get("firstName"),(String)tmp.get("lastName"));
+			res[j] = hp;
+			j++;
+        }
+        
+        mongoClient.close();
+		return res;
+
 	}
 	
 	/**
@@ -160,9 +304,16 @@ public class HistoryController extends ParentController{
 	public double  getItemsSimilarity(@RequestParam("title1") String title1,
 			@RequestParam("title2") String title2){
 		//:TODO your implementation
-		double ret = 0.0;
+        int j = 0;
+        for(User i : getUsersByItem(title1)) {
+        	for(User k : getUsersByItem(title2)) {      		
+        		if(i.getUsername().equals(k.getUsername())) {
+        			j++;
+        		}
+        	}
+        }
+        	
+		double ret = (double)j/(getUsersByItem(title1).length+getUsersByItem(title2).length);
 		return ret;
 	}
-	
-
 }
